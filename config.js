8ad2e797850064189673c200cfd7cbfb0fbd92b7
Kevin Ennis <kennis84@gmail.com>
2018-09-17 09:20:07 -0400

    Add hasroles directive

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ module.exports = {
   database: {
     hosts: [ 'localhost:27017' ],
     database: 'graphqltest'
-  }
+  },
+
+  secret: 'graphqltestsecret'
 
 };
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express     = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema      = require('./lib/schema');
+const config      = require('./config');
+const cookie      = require('cookie-parser');
+const jwt         = require('jsonwebtoken');
 
 const app = express();
+
+app.use( cookie() );
+
+// set an auth token for everyone (obv just for testing purposes)
+app.use( ( req, res, next ) => {
+  const token = jwt.sign({
+    user_id: '555555555555555555555555',
+    roles: [
+      'users:list',
+      'users:show',
+      'users:create',
+      'users:update',
+      'users:read_name'
+    ]
+  }, config.secret );
+
+  res.cookie( 'graphqlauth', token );
+
+  next();
+});
+
+// parse auth cookies
+app.use( ( req, res, next ) => {
+  try {
+    req.token = jwt.verify( req.cookies.graphqlauth, config.secret );
+  } catch ( err ) {}
+
+  return next();
+});
 
 app.use( '/graphql', graphqlHTTP({ schema, graphiql: true }) );
 
